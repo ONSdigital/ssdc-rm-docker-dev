bb487f9fac19168987945c17200d7bd9294e12a0
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-08-02 15:44:57 +0100

    pubsub naming: can't use full stops

--- setup_pubsub.sh
+++ setup_pubsub.sh
@@ -12,38 +12,38 @@ curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/$RECEIPT_TOPIC_PROJECT_ID/subs
 # Below are the minimum RM topics & events - ideally external systems will publish/subscribe to these, in the correct standardised format
 
 curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/topics/rm-internal-telephone-capture
-curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/rm-internal-telephone-capture.case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/rm-internal-telephone-capture"}'
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/rm-internal-telephone-capture_case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/rm-internal-telephone-capture"}'
 
 curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/topics/rm-internal-sample
-curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/rm-internal-sample.case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/rm-internal-sample"}'
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/rm-internal-sample_case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/rm-internal-sample"}'
 
-curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/topics/event.receipt
-curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/event.receipt.rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/event.receipt"}'
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/topics/event_receipt
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/event_receipt_rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/event_receipt"}'
 
-curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/topics/event.refusal
-curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/event.refusal.rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/event.refusal"}'
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/topics/event_refusal
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/event_refusal_rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/event_refusal"}'
 
 curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/topics/rm-internal-print-row
-curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/rm-internal-print-row.print-file-service -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/rm-internal-print-row"}'
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/rm-internal-print-row_print-file-service -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/rm-internal-print-row"}'
 
-curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/topics/event.invalid
-curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/event.invalid.rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/event.invalid"}'
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/topics/event_invalid
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/event_invalid_rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/event_invalid"}'
 
-curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/topics/event.survey-launched
-curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/event.survey-launched.rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/event.survey-launched"}'
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/topics/event_survey-launched
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/event_survey-launched_rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/event_survey-launched"}'
 
-curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/topics/event.paper-fulfilment
-curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/event.paper-fulfilment.rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/event.paper-fulfilment"}'
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/topics/event_paper-fulfilment
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/event_paper-fulfilment_rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/event_paper-fulfilment"}'
 
-curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/topics/event.deactivate-uac
-curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/event.deactivate-uac.rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/event.deactivate-uac"}'
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/topics/event_deactivate-uac
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/event_deactivate-uac_rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/event_deactivate-uac"}'
 
-curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/topics/event.update-sample-sensitive
-curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/event.update-sample-sensitive.rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/event.update-sample-sensitive"}'
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/topics/event_update-sample-sensitive
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/event_update-sample-sensitive_rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/event_update-sample-sensitive"}'
 
-curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/topics/event.case-update
-curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/event.case-update.rh -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/event.case-update"}'
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/topics/event_case-update
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/event_case-update_rh -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/event_case-update"}'
 
-curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/topics/event.uac-update
-curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/event.uac-update.rh -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/event.uac-update"}'
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/topics/event_uac-update
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/event_uac-update_rh -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/event_uac-update"}'
 
