2c86683309f2e660bf93506abd2ae4aa96310b62
AdamHawtin <adam.hawtin@ons.gov.uk>
2021-08-09 15:10:30 +0100

    Change subscription name

--- setup_pubsub.sh
+++ setup_pubsub.sh
@@ -42,4 +42,4 @@ curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/topics/event_uac-updat
 curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/event_uac-update_rh -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/event_uac-update"}'
 
 curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/topics/rm-internal-sms-fulfilment
-curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/rm-internal-sms-fulfilment_rm-case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/rm-internal-sms-fulfilment"}'
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/project/subscriptions/rm-internal-sms-fulfilment_case-processor -H 'Content-Type: application/json' -d '{"topic": "projects/project/topics/rm-internal-sms-fulfilment"}'
