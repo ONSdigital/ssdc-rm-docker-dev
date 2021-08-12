ba900e0f1e2efcff3830d8c6f4cc039c35753d20
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-08-12 12:36:37 +0100

    Update publish_message.sh

--- publish_message.sh
+++ publish_message.sh
@@ -11,4 +11,4 @@ source .env
 echo "Pipe in message to send to topic '$1', or paste and it ctrl-d when done"
 message=$(cat)
 
-PUBSUB_EMULATOR_HOST=$PUBSUB_SETUP_HOST python publish_message.py $1 $message
+PUBSUB_EMULATOR_HOST=$PUBSUB_SETUP_HOST pipenv run python publish_message.py $1 $message
