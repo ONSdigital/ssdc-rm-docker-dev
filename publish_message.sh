177daef46bf9720d8e2e1cea742ceba37a460d1a
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-08-12 16:07:58 +0100

    fix script

--- publish_message.sh
+++ publish_message.sh
@@ -6,6 +6,4 @@ then
     exit -1
 fi
 
-source .env
-
-PIPENV_DONT_LOAD_ENV=1 PUBSUB_EMULATOR_HOST=$PUBSUB_SETUP_HOST pipenv run python publish_message.py $1
+PIPENV_DONT_LOAD_ENV=1 PUBSUB_EMULATOR_HOST=localhost:8538 pipenv run python publish_message.py $1
