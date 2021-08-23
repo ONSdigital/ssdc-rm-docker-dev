727f6aa30d98cc11d4cdb81075a81f86a3d938be
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-08-23 14:37:37 +0100

    Add clear script

---
+++ clear_messages.py
@@ -0,0 +1,38 @@
+import argparse
+
+from google.api_core.exceptions import DeadlineExceeded
+from google.cloud import pubsub_v1
+
+
+def clear_messages(project, subscription):
+    subscriber = pubsub_v1.SubscriberClient()
+    subscription_path = subscriber.subscription_path(project, subscription)
+
+    try:
+        response = subscriber.pull(subscription_path, max_messages=1000, timeout=30)
+
+        if not response.received_messages:
+            print("No messages on subscription")
+    except DeadlineExceeded:
+        print("No messages on subscription")
+        return
+
+    ack_ids = [message.ack_id for message in response.received_messages]
+
+    if ack_ids:
+        subscriber.acknowledge(subscription_path, ack_ids)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description='Clear messages from a Google Pub/Sub subscription (or the emulator)')
+    parser.add_argument('subscription', help='The subscription clear messages from', type=str)
+    parser.add_argument('--project', help='The project the subscription belongs to',
+                        type=str, default='project')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+
+    clear_messages(args.project, args.subscription)
---
+++ clear_messages.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]
+then
+    echo "Usage: clear_messages.sh <PROJECT> <SUBSCRIPTION>"
+    exit -1
+fi
+
+PIPENV_DONT_LOAD_ENV=1 PUBSUB_EMULATOR_HOST=localhost:8538 pipenv run python clear_messages.py $1 --project $2
